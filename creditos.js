5eaab0aaf24ae513c0425a5c453527f6f478ba8c
acg2002 <40214877+acg2002@users.noreply.github.com>
2018-11-29 09:30:45 -0200

    Add files via upload

--- creditos.js
+++ creditos.js
@@ -6,16 +6,19 @@ tt = $('#tt');
 $artCred.click(function(){
     fb.attr('href', 'https://www.facebook.com/arthur.carvalhoguerra');
     inst.attr('href', 'https://www.instagram.com/arthur_cg2/?hl=pt-br');
+    inst.css('display', 'inline-block');
     tt.css('display', 'none');
 });
 $enzCred.click(function(){
     fb.attr('href', 'https://www.facebook.com/enzo.rodrigues.754570');
     inst.css('display', 'none');
     tt.attr('href', 'https://twitter.com/ZE_ROB3RTO');
+    tt.css('display', 'inline-block');
 });
 $iagCred.click(function(){
     fb.attr('href', 'https://www.facebook.com/iago.gabinogoncalves');
     inst.attr('href', 'https://www.instagram.com/iagogabino/?hl=pt-br');
+    inst.css('display', 'inline-block');
     tt.css('display', 'none');
 });
 $gabCred.click(function(){
